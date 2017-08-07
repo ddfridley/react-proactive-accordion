1766beb684ab769408c460f61a41618cc6d4b08f
David Fridley <ddfridley@yahoo.com>
2017-08-07 15:59:51 -0700

    ls in postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 mkdir -p dist/demo
+ls -ld node_modules/babel*
 babel --out-dir dist src &&
 browserify dist/demo.js -o dist/demo/demo.js && 
 browserify dist/react-proactive-accordion.js -o dist/demo/react-proactive-accordion.js &&
